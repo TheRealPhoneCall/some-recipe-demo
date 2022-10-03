bbc0e869591dce749a34a3d571b5830a6c7fdd03
Daryl Pongcol <pongcoldaryl@gmail.com>
2022-10-03 16:33:01 +0800

    removed unnecessaries

--- frontend/quasar.config.js
+++ frontend/quasar.config.js
@@ -29,7 +29,6 @@ module.exports = configure(function (/* ctx */) {
     // https://v2.quasar.dev/quasar-cli-vite/boot-files
     boot: [
       'axios'
-      // 'maps'
     ],
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#css
